14d1750ba307c5242b2c0b6486690faf3ffa351a
David Haberthür <email@davidhaberthuer.ch>
2022-04-12 18:09:11 +0200

    Also sync from/to FastSSD on anaklin25
    Former-commit-id: 72af59a46c88495bf1e82a1623523a1332ed3036

--- rsync-dea-beckenboden.sh
+++ rsync-dea-beckenboden.sh
@@ -1,6 +1,12 @@
 # 2214 -> resstore dea
 echo "2214 -> resstore dea"
-rsync --verbose --recursive --update --omit-dir-times ~/2214/Beckenboden\ Dea/ ~/research-storage-djonov/Aaldijk
+rsync --verbose --recursive --update --times --omit-dir-times --exclude="*.?if" ~/2214/Beckenboden\ Dea/ ~/research-storage-djonov/Aaldijk/
 # 2214 -> Archiv
 echo "2214 -> archiv"
-rsync --verbose --recursive --update --omit-dir-times --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*_spr*.bmp" --include="*.txt" --include="*.md" --include="*.sb" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.?if" --exclude="*" ~/2214/Beckenboden\ Dea/ ~/research-storage-uct/Archiv_Tape/Aaldijk\ Beckenboden/
+rsync --verbose --recursive --update --times --omit-dir-times --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*_spr*.bmp" --include="*.txt" --include="*.md" --include="*.sb" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.?if" --exclude="*" ~/2214/Beckenboden\ Dea/ ~/research-storage-uct/Archiv_Tape/Aaldijk\ Beckenboden/
+echo "2214 -> FastSSD (excluding some stuff)"
+rsync --verbose --recursive --update --times --omit-dir-times --exclude="Foetus01*" --exclude="rec_old" --exclude="*.?if" ~/2214/Beckenboden\ Dea/ /media/habi/Fast_SSD/Aaldijk/
+echo "resstore dea -> FastSSD (excluding some stuff)"
+rsync --verbose --recursive --update --times --omit-dir-times  --exclude="Foetus01*" --exclude="rec_old" --exclude="*.?if" ~/research-storage-djonov/Aaldijk/ /media/habi/Fast_SSD/Aaldijk/
+echo "FastSSD --> resstore dea"
+rsync --verbose --recursive --update --times --omit-dir-times /media/habi/Fast_SSD/Aaldijk/ ~/research-storage-djonov/Aaldijk/
