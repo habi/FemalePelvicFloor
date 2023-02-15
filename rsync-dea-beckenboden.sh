9f1fd1d3ce574eadffc823100c1e9079129ca1f5
David Haberthür <email@davidhaberthuer.ch>
2023-02-15 12:40:41 +0100

    Copy all log files into subfolder here
    Former-commit-id: 0b5de341cfc20337ee660bfd77e7acfa0dd116c9
    Former-commit-id: a0766d49047f95db632d4e4f2c60022190e05159

--- rsync-dea-beckenboden.sh
+++ rsync-dea-beckenboden.sh
@@ -4,9 +4,11 @@ rsync --verbose --recursive --update --times --omit-dir-times --exclude="*.?if" 
 # 2214 -> Archiv
 echo "2214 -> archiv"
 rsync --verbose --recursive --update --times --omit-dir-times --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*_spr*.bmp" --include="*.txt" --include="*.md" --include="*.sb" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.?if" --exclude="*" ~/2214/Beckenboden\ Dea/ ~/research-storage-uct/Archiv_Tape/Aaldijk\ Beckenboden/
-echo "2214 -> FastSSD (excluding some stuff)"
-rsync --verbose --recursive --update --times --omit-dir-times --exclude="Foetus01*" --exclude="rec_old" --exclude="*.?if" ~/2214/Beckenboden\ Dea/ /media/habi/Fast_SSD/Aaldijk/
-echo "resstore dea -> FastSSD (excluding some stuff)"
-rsync --verbose --recursive --update --times --omit-dir-times  --exclude="Foetus01*" --exclude="rec_old" --exclude="*.?if" ~/research-storage-djonov/Aaldijk/ /media/habi/Fast_SSD/Aaldijk/
-echo "FastSSD --> resstore dea"
-rsync --verbose --recursive --update --times --omit-dir-times /media/habi/Fast_SSD/Aaldijk/ ~/research-storage-djonov/Aaldijk/
+# echo "2214 -> FastSSD (excluding some stuff)"
+# rsync --verbose --recursive --update --times --omit-dir-times --exclude="Foetus01*" --exclude="rec_old" --exclude="*.?if" ~/2214/Beckenboden\ Dea/ /media/habi/Fast_SSD/Aaldijk/
+# echo "resstore dea -> FastSSD (excluding some stuff)"
+# rsync --verbose --recursive --update --times --omit-dir-times  --exclude="Foetus01*" --exclude="rec_old" --exclude="*.?if" ~/research-storage-djonov/Aaldijk/ /media/habi/Fast_SSD/Aaldijk/
+# echo "FastSSD --> resstore dea"
+# rsync --verbose --recursive --update --times --omit-dir-times /media/habi/Fast_SSD/Aaldijk/ ~/research-storage-djonov/Aaldijk/
+echo "Log files and stuff on 2214 -> Repository subfolder"
+rsync --verbose --recursive --update --times --omit-dir-times --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.txt" --include="*.md" --include="*.?nc" --exclude="*" ~/2214/Beckenboden\ Dea/ /home/habi/P/Documents/Dea-Beckenboden/logfiles/
