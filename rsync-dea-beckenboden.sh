0d778d7a544d3a2eb57b00bbfe8b5d7883a83de3
David Haberthür <email@davidhaberthuer.ch>
2024-09-11 08:09:57 +0200

    Tweak paths and exclude Imaris files

--- rsync-dea-beckenboden.sh
+++ rsync-dea-beckenboden.sh
@@ -1,15 +1,15 @@
 # 2214 -> resstore dea
 echo "2214 -> resstore dea"
-rsync --verbose --recursive --update --times --omit-dir-times --exclude="*_rectmp.log" --exclude="*.?if" ~/2214/Beckenboden\ Dea/ ~/research-storage-djonov/Aaldijk/PelvicFloor/
+rsync --verbose --recursive --update --times --omit-dir-times --exclude="*_rectmp.log" --exclude="*.?if" ~/2214/Beckenboden\ Dea/ ~/research_storage_djonov/Aaldijk/PelvicFloor/
 # 2214 -> Archiv
 echo "2214 -> archiv"
-rsync --verbose --recursive --update --times --omit-dir-times --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.?at" --include="*_spr*.bmp" --include="*.txt" --include="*.md" --include="*.sb" --include="*.info" --include="*.?nc" --include="*.bkp" --include="*.?if" --exclude="*_rectmp.log" --exclude="*" ~/2214/Beckenboden\ Dea/ ~/research-storage-uct/Archiv_Tape/Aaldijk\ Beckenboden/
+rsync --verbose --recursive --times --update --omit-dir-times --prune-empty-dirs --exclude="*rec*.png" --exclude="*.dcm" --exclude="*.zarr" --exclude="*_rectmp.log" --exclude="SubScan?" --exclude="Thumbs.db" ~/2214/Beckenboden\ Dea/ ~/research_storage_uct/Archiv_Tape/Aaldijk\ Beckenboden/
 echo "Generate copy on FastSSD"
 echo "2214 --> FastSSD"
-rsync --verbose --recursive --update --times --omit-dir-times --exclude="*_rectmp.log" --exclude="*.?if" ~/2214/Beckenboden\ Dea/ /media/habi/Fast_SSD/Aaldijk/
+rsync --verbose --recursive --update --times --omit-dir-times --exclude="*_rectmp.log" --exclude="*.?if" ~/2214/Beckenboden\ Dea/ /media/habi/Fast_SSD/Aaldijk/PelvicFloor/
 echo "resstore dea --> FastSSD"
-rsync --verbose --recursive --update --times --omit-dir-times --exclude="*_rectmp.log" --exclude="*.raw" --exclude="*.?if" ~/research-storage-djonov/Aaldijk/PelvicFloor /media/habi/Fast_SSD/Aaldijk/
+rsync --verbose --recursive --update --times --omit-dir-times --exclude="*_rectmp.log" --exclude="*.raw" --exclude="*.?if" --exclude="*.ims" ~/research_storage_djonov/Aaldijk/PelvicFloor/ /media/habi/Fast_SSD/Aaldijk/PelvicFloor/
 echo "Update  resstore dea from FastSSD"
-rsync --verbose --recursive --update --times --omit-dir-times /media/habi/Fast_SSD/Aaldijk/ ~/research-storage-djonov/Aaldijk/PelvicFloor
+rsync --verbose --recursive --update --times --omit-dir-times /media/habi/Fast_SSD/Aaldijk/PelvicFloor/ ~/research_storage_djonov/Aaldijk/PelvicFloor/
 echo "Log files and stuff on 2214 -> Repository subfolder"
 rsync --verbose --recursive --update --times --omit-dir-times --include="*/" --include="*.?og" --include="*.c?v" --include="*.?oi" --include="*.txt" --include="*.md" --include="*.?nc" --exclude="*_rectmp.log" --exclude="*" ~/2214/Beckenboden\ Dea/ /home/habi/P/Documents/Dea-Beckenboden/logfiles/
